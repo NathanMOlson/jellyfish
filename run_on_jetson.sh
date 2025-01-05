3ecfd90235399abd571085bc696c95dbca843c3a
Nathan <nathanmolson@gmail.com>
2025-01-05 11:37:37 -0800

    update jetson script

--- run_on_jetson.sh
+++ run_on_jetson.sh
@@ -1,2 +1,2 @@
 sudo modprobe nvidia-drm modeset=1
-DISPLAY=:0 gst-launch-1.0 filesrc location=jelly.mp4 ! qtdemux ! h264parse ! nvv4l2decoder ! queue ! nvvidconv ! video/x-raw ! videocrop right=70 ! jellyfilter ! nv3dsink --gst-plugin-path=/home/formapath/jellyfish/build
+DISPLAY=:0 gst-launch-1.0 filesrc location=jelly.mp4 ! qtdemux ! h264parse ! nvv4l2decoder ! queue ! nvvidconv ! video/x-raw ! videocrop right=70 ! jellyfilter ! videoscale ! video/x-raw,width=1920,height=1080 ! nvdrmvideosink --gst-plugin-path=/home/formapath/jellyfish/build
